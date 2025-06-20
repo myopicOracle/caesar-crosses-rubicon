9d7ef822e585b78bcfb36a79dd27d292b7302b3f
myopicOracle <184602237+myopicOracle@users.noreply.github.com>
2025-06-20 03:53:59 -0700

    feat: useCypher() function correctly shifts by specified amount

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
-const cypher = 4
+const alphabet26 = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+const alphabet52 = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+const display = document.querySelector("#app")
+
+
+let testString = "I shall cross the Rubicon by mid-night of tonight."
+const cypherShift = 4
+
+const useCypher = (string) => {
+//   console.log("string: ", string)
+
+  const originalString = string
+  const stringToArray = splitString(originalString)
+//   console.log("stringToArray: ", stringToArray)
+
+  const encryptedArray = stringToArray.map((char) => {
+
+    const charIndex = alphabet52.findIndex((letter) => letter === char.toUpperCase())
+    // console.log("charIndex: ", charIndex)
+
+    const shiftedIndex = charIndex + cypherShift
+    // console.log("shiftedIndex: ", shiftedIndex)
+
+    const newChar = alphabet52[shiftedIndex]
+    // console.log("newChar: ", newChar)
+
+    if (char !== ' ') {
+      return newChar
+    } else {
+      return char
+    }
+  
+  })
+  
+  const encryptedString = joinArray(encryptedArray)
+//   console.log("encryptedString: ", encryptedString)
+  
+  return encryptedString
+}
+
+const splitString = (string) => {
+  return string.split("")
+}
+
+const joinArray = (string) => {
+  return string.join("")
+}
+
+console.log("useCypher: ", useCypher(testString))
